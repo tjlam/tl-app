e8c2493686b193964b4fb26b815cc03ab4e53ccd
Tyler Lam <tylerlam888@gmail.com>
2020-09-10 01:36:21 -0400

    did things

--- idk/main.js
+++ idk/main.js
@@ -33,3 +33,23 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+const ipc = require('node-ipc');
+
+ipc.config.id = 'main';
+ipc.config.retry = 1500;
+
+ipc.serve(() => {
+    ipc.server.on('SPOTIFY_DATA', (message, socket) => {
+        console.log(message);
+    });
+
+    ipc.server.on('socket.disconnected', () => {
+        ipc.log('client disconnected');
+    });
+
+    ipc.server.on('error', (err) => {
+        console.log(err);
+    })
+});
+ipc.server.start();
