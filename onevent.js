f4ee2feae1df79247541eb54066ec9972b89dc05
Tyler Lam <tylerlam888@gmail.com>
2020-09-12 23:23:27 -0400

    iueno

--- onevent.js
+++ onevent.js
@@ -1,9 +1,9 @@
-const { fork } = require('child_process');
+const { exec } = require('child_process');
 const path = require('path');
 
 const {PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS} = process.env;
 console.log(`process.env`, process.env);
 
-const p = fork(path.join(__dirname, 'spotify_publisher.js'), [PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS], {
-    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+const p = exec(`sudo node ${path.join(__dirname, 'spotify_publisher.js')}`, {
+    env: process.env
 });
--- spotify_publisher.js
+++ spotify_publisher.js
@@ -21,14 +21,14 @@ ipc.config.id = 'publisher';
 ipc.config.retry = 1500;
 ipc.config.maxRetries = 2;
 
-const args = process.argv.slice(2);
-console.log(`args`, args);
+const {PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS} = process.env;
 const spotifyData = {
-    PLAYER_EVENT: args[0],
-    TRACK_ID: args[1],
-    DURATION_MS: args[2],
-    POSITION_MS: args[3]
+  PLAYER_EVENT,
+  TRACK_ID,
+  DURATION_MS,
+  POSITION_MS
 };
+console.log(`spotify data`, spotifyData);
 
 ipc.connectTo('main', '/tmp/app.main', () => {
   ipc.of.main.on('connect', () => {
