749cf3d38b609c33c420a4c594decd96df43dcb6
Tyler Lam <tylerlam888@gmail.com>
2020-09-12 23:00:18 -0400

    test thsi

---
+++ onevent.js
@@ -0,0 +1,8 @@
+const { fork } = require('child_process');
+const path = require('path');
+
+const {PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS} = process.env;
+
+const p = fork(path.join(__dirname, 'spotify_publisher.js'), [PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS], {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+});
--- spotify_publisher.js
+++ spotify_publisher.js
@@ -21,12 +21,12 @@ ipc.config.id = 'publisher';
 ipc.config.retry = 1500;
 ipc.config.maxRetries = 2;
 
-const {PLAYER_EVENT, TRACK_ID, DURATION_MS, POSITION_MS} = process.env;
+const args = process.argv.slice(2);
 const spotifyData = {
-    PLAYER_EVENT,
-    TRACK_ID,
-    DURATION_MS,
-    POSITION_MS
+    PLAYER_EVENT: args[0],
+    TRACK_ID: args[1],
+    DURATION_MS: args[2],
+    POSITION_MS: args[3]
 };
 
 ipc.connectTo('main', '/tmp/app.main', () => {
